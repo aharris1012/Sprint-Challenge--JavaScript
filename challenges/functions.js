523e6aa87602f1b509eeaa6fd96023df298dd719
Andrea Harris <Andrea@zacs-imac.lan>
2019-11-15 16:32:38 -0600

    length typo fixed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,8 +6,10 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-function consume(a,b,cb);
-return cb(a,b);
+function consume(a,b,cb){
+  return cb(a,b)
+}
+
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -21,8 +23,8 @@ function add (a,b){
 function multiply(a,b){
   return a*b;
 }
-function gretting(first, last){
-  return "Hello" +''+first + '' +last+',' `nice to meet you!`;
+function greeting(first, last){
+  return "Hello" +' '+first + ' ' +last+' ' `nice to meet you!`;
 }
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
  console.log(consume(2, 2, add)); // 4
